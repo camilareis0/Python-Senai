8933f28bf3a2221f846aeb732814b61d316f82f8
camilareis0 <camilareis783@gmail.com>
2024-12-03 09:54:00 -0300

    Aula 2

---
+++ Aula 2/Aula2.py
@@ -0,0 +1,14 @@
+print('Olá, tudo bem? ')
+
+nome_usuari0 = 'Camila Reis' # kjjhhh
+idade_usuario = 19 
+altura_usuario = 1.60 # tipo = float = ponto flutuante 
+maior_idade = True # tipo = bool = boolean
+
+print('Seja bem-vindo,', nome_usuari0)
+
+print("Sua idade é", idade_usuario, "anos")
+
+print("Você tem", altura_usuario, "metros.")
+
+print("Você é maior de idade?", maior_idade)
